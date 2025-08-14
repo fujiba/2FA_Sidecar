cd1bdb2dcd48517b20f601ad60be399ad8e35a24
Toshihiro Fujibayashi <fujiba@fujiba.net>
2025-08-14 14:58:57 +0900

    mini版での動作可能バージョン

--- 2FA_Sidecar.ino
+++ 2FA_Sidecar.ino
@@ -1,14 +1,14 @@
 // 2FA Sidecar
 // Matt Perkins - Copyright (C) 2024
 // Spawned out of the need to often type a lot of two factor authentication
-// but still have some security while remaning mostly isolated from the host system.
-// See github for 3D models and wiring diagram.
+// but still have some security while remaning mostly isolated from the host
+// system. See github for 3D models and wiring diagram.
 /*
 
-    This program is free`                      nmjhsoftware: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This program is free`                      nmjhsoftware: you can
+   redistribute it and/or modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,55 +20,47 @@
 
 */
 
-// Change for your country. 
-#define NTP_SERVER "au.pool.ntp.org" //Adjust to your local time perhaps. 
-#define TZ "AEST" // Australian Estern time may be needed for clock display in the future. 
+// Change for your country.
+// Adjust to your local time perhaps.
+#define NTP_SERVER "ntp.jst.mfeed.ad.jp"
+// Japan standard time may be needed for clock display in the future.
+#define TZ "JST-9"
 
+// Only other thing to select bellow is if your making the 5 key or 2 key
+// version Default is 5 key.
 
+const char *mainver = "1.11";
 
-// Only other thing to select bellow is if your making the 5 key or 2 key version
-// Default is 5 key.  
-
-
-char *mainver = "1.11";
-
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <Adafruit_GFX.h>     // Core graphics library
+#include <Adafruit_ST7789.h>  // Hardware-specific library for ST7789
+#include <Preferences.h>      // perstant storage
 #include <SPI.h>
-#include <Preferences.h> // perstant storage
 
 // Misc Fonts
-#include "Fonts/FreeSans9pt7b.h"
-#include "Fonts/FreeSans12pt7b.h"
-#include "Fonts/FreeSans18pt7b.h"
-#include "Fonts/FreeMono12pt7b.h"
-
-#include <string>
-
-
-
-#include <PinButton.h> // Button Library 
-
+#include <Arduino.h>
+#include <PinButton.h>  // Button Library
 #include <USB.h>
 #include <USBHIDKeyboard.h>
 
-#include <Arduino.h>
+#include <string>
+
+#include "Fonts/FreeMono12pt7b.h"
+#include "Fonts/FreeSans12pt7b.h"
+#include "Fonts/FreeSans18pt7b.h"
+#include "Fonts/FreeSans9pt7b.h"
 #ifdef ESP32
-#include <WiFi.h>
 #include <AsyncTCP.h>
+#include <WiFi.h>
 #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #endif
-#include <ESPAsyncWebSrv.h>
-
-
-#include <lwip/apps/sntp.h>
-#include <TOTP-RC6236-generator.hpp>
-#include <ESPmDNS.h>
 #include <ArduinoOTA.h>
+#include <ESPAsyncWebSrv.h>
+#include <ESPmDNS.h>
+#include <lwip/apps/sntp.h>
 
-
+#include <TOTP-RC6236-generator.hpp>
 
 // Init HID so we look like a keyboard
 USBHIDKeyboard Keyboard;
@@ -78,24 +70,22 @@ int bargraph_pos;
 int updateotp;
 long time_x;
 
-
 PinButton key1(5);
 PinButton key2(6);
 PinButton key3(9);
 PinButton key4(10);
 PinButton key5(11);
 
-
 int keytest = 0;
 
-// Select either the 5 key or 2 key version 
-// maxkeys = 15 for 5 key 
-// maxkeys = 3 for 2 key 
+// Select either the 5 key or 2 key version
+// maxkeys = 15 for 5 key
+// maxkeys = 3 for 2 key
 
-int maxkeys = 15; 
+int maxkeys = 3;
 int sline = 0;
 int pinno = 0;
-String  in_pin;
+String in_pin;
 
 // Incorrect Pin Delay
 int pindelay = 3000;
@@ -103,9 +93,9 @@ int pindelay = 3000;
 AsyncWebServer server(80);
 
 // Setup SSID
-String ssid     = "Key-Sidecar";
+String ssid = "Key-Sidecar";
 String password;
-String  pin;
+String pin;
 
 String tfa_name_1;
 String tfa_seed_1;
@@ -123,28 +113,25 @@ String tfa_name_5;
 String tfa_seed_5;
 
 // Paramaters wifi
-const char* PARAM_INPUT_1 = "ssid";
-const char* PARAM_INPUT_2 = "password";
-const char* PARAM_INPUT_3 = "pin";
+const char *PARAM_INPUT_1 = "ssid";
+const char *PARAM_INPUT_2 = "password";
+const char *PARAM_INPUT_3 = "pin";
 
 // Paramaters 2FA
-const char* TFA_INPUT_1 = "tfa_name_1";
-const char* TFA_INPUT_2 = "tfa_seed_1";
+const char *TFA_INPUT_1 = "tfa_name_1";
+const char *TFA_INPUT_2 = "tfa_seed_1";
 
-const char* TFA_INPUT_3 = "tfa_name_2";
-const char* TFA_INPUT_4 = "tfa_seed_2";
+const char *TFA_INPUT_3 = "tfa_name_2";
+const char *TFA_INPUT_4 = "tfa_seed_2";
 
-const char* TFA_INPUT_5 = "tfa_name_3";
-const char* TFA_INPUT_6 = "tfa_seed_3";
+const char *TFA_INPUT_5 = "tfa_name_3";
+const char *TFA_INPUT_6 = "tfa_seed_3";
 
-const char* TFA_INPUT_7 = "tfa_name_4";
-const char* TFA_INPUT_8 = "tfa_seed_4";
+const char *TFA_INPUT_7 = "tfa_name_4";
+const char *TFA_INPUT_8 = "tfa_seed_4";
 
-const char* TFA_INPUT_9 = "tfa_name_5";
-const char* TFA_INPUT_10 = "tfa_seed_5";
-
-
-
+const char *TFA_INPUT_9 = "tfa_name_5";
+const char *TFA_INPUT_10 = "tfa_seed_5";
 
 // Init Screen
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
@@ -152,9 +139,7 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 // Init Preferences
 Preferences preferences;
 
-
 void setup() {
-
   // turn on backlite
   pinMode(TFT_BACKLITE, OUTPUT);
   digitalWrite(TFT_BACKLITE, HIGH);
@@ -165,7 +150,7 @@ void setup() {
   delay(10);
 
   // initialize TFT
-  tft.init(135, 240); // Init ST7789 240x135
+  tft.init(135, 240);  // Init ST7789 240x135
   tft.setRotation(3);
   tft.fillScreen(ST77XX_BLACK);
   tft.setFont(&FreeSans9pt7b);
@@ -178,9 +163,8 @@ void setup() {
   tft.printf("Press K1 to enter config/test\n");
 
   // Check for key and go to setup /test mode
-  int lcount = 0 ;
+  int lcount = 0;
   while (lcount < 140) {
-
     key1.update();
 
     if (key1.isClick()) {
@@ -203,7 +187,6 @@ void setup() {
 
   ssid = preferences.getString("ssid", "");
   password = preferences.getString("password", "");
-
 
   // load all pramaters
   tfa_name_1 = preferences.getString("tfa_name_1", "");
@@ -232,7 +215,7 @@ void setup() {
       tft.fillScreen(ST77XX_BLACK);
       tft.setTextColor(ST77XX_WHITE);
       tft.setCursor(3, 5);
-      sline = 0 ;
+      sline = 0;
     }
   }
   tft.print("IP: ");
@@ -244,14 +227,14 @@ void setup() {
   configTzTime(TZ, NTP_SERVER);
   tft.println();
   tft.printf("NTP started:%s", TZ);
-  time_t t = time(NULL);
-  tft.printf(":%d", t);
-  tft.println();
 
+  time_t t = time(NULL);
+  tft.printf(":%lld", t);
+  tft.println();
 
   // Check to seee if we have PIN set and ask if we do.
   pin = preferences.getString("pin", "");
-  const char* npin = pin.c_str();
+  const char *npin = pin.c_str();
   if (strlen(npin) > 3) {
     tft.setCursor(25, 25);
     tft.setFont(&FreeSans12pt7b);
@@ -303,7 +286,7 @@ void setup() {
         tft.print("*");
       }
 
-      if ( pinno == 4) {
+      if (pinno == 4) {
         if (in_pin == npin) {
           tft.println();
           tft.println("Correct.");
@@ -317,13 +300,9 @@ void setup() {
       }
     }
 
-
-
-  } // end check PIN
-
+  }  // end check PIN
 
   tft.setTextColor(ST77XX_WHITE);
-
 
   // Iniz Keyboard and begin display.
   tft.setFont(&FreeSans9pt7b);
@@ -335,9 +314,6 @@ void setup() {
   tft.fillScreen(ST77XX_BLACK);
   tft.setTextColor(ST77XX_WHITE);
   updateotp = 1;
-
-
-
 }
 
 void loop() {
@@ -353,8 +329,6 @@ void loop() {
 
   lst = millis();
 
-
-
   // Update Time.
 
   time_t t = time(NULL);
@@ -364,24 +338,20 @@ void loop() {
     return;
   };
 
-
-
   bargraph_pos = (t % 60);
   if (bargraph_pos > 29) {
     bargraph_pos = bargraph_pos - 30;
   }
 
   bargraph_pos = bargraph_pos * 3;
-  if (bargraph_pos < 70) {
-    tft.fillCircle(bargraph_pos, 125, 3, ST77XX_GREEN);
-  } else {
-    tft.fillCircle(bargraph_pos, 125, 3, ST77XX_RED);
-  }
+  tft.fillCircle(bargraph_pos + 10, 125, 3,
+                 bargraph_pos < 70 ? ST77XX_GREEN : ST77XX_RED);
+
+  tft.fillRect(0, 0, 2, 125, ST77XX_WHITE);
 
   if (bargraph_pos == 0) {
     updateotp = 1;
   }
-
 
   // Display updated OTP per key
 
@@ -392,7 +362,7 @@ void loop() {
     tft.fillScreen(ST77XX_BLACK);
 
     // Key 1
-    if (String * otp1 = TOTP::currentOTP(tfa_seed_1)) {
+    if (String *otp1 = TOTP::currentOTP(tfa_seed_1)) {
       tft.setCursor(3, 17);
       tft.setTextColor(ST77XX_RED);
       tft.setFont(&FreeSans12pt7b);
@@ -407,11 +377,10 @@ void loop() {
       tft.print("NO VALID CONFIG");
       delay(10000);
       ESP.restart();
-
     };
 
     // Key 2
-    if (String * otp2 = TOTP::currentOTP(tfa_seed_2)) {
+    if (String *otp2 = TOTP::currentOTP(tfa_seed_2)) {
       tft.setCursor(3, 40);
       tft.setTextColor(ST77XX_RED);
       tft.setFont(&FreeSans12pt7b);
@@ -423,7 +392,7 @@ void loop() {
     };
 
     // Key 3
-    if (String * otp3 = TOTP::currentOTP(tfa_seed_3)) {
+    if (String *otp3 = TOTP::currentOTP(tfa_seed_3)) {
       tft.setCursor(3, 63);
       tft.setTextColor(ST77XX_RED);
       tft.setFont(&FreeSans12pt7b);
@@ -435,7 +404,7 @@ void loop() {
     };
 
     // Key 4
-    if (String * otp4 = TOTP::currentOTP(tfa_seed_4)) {
+    if (String *otp4 = TOTP::currentOTP(tfa_seed_4)) {
       tft.setCursor(3, 86);
       tft.setTextColor(ST77XX_RED);
       tft.setFont(&FreeSans12pt7b);
@@ -447,7 +416,7 @@ void loop() {
     };
 
     // Key 5
-    if (String * otp5 = TOTP::currentOTP(tfa_seed_5)) {
+    if (String *otp5 = TOTP::currentOTP(tfa_seed_5)) {
       tft.setCursor(3, 109);
       tft.setTextColor(ST77XX_RED);
       tft.setFont(&FreeSans12pt7b);
@@ -460,13 +429,11 @@ void loop() {
 
     // Make up the rest of the second so we dont fliker the screen.
     delay(999);
-
   }
-
 
   // check keypress
   if (key1.isClick()) {
-    String * otp1 = TOTP::currentOTP(tfa_seed_1);
+    String *otp1 = TOTP::currentOTP(tfa_seed_1);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
     Keyboard.println(*otp1);
@@ -474,7 +441,7 @@ void loop() {
   }
 
   if (key2.isClick()) {
-    String * otp2 = TOTP::currentOTP(tfa_seed_2);
+    String *otp2 = TOTP::currentOTP(tfa_seed_2);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
     Keyboard.println(*otp2);
@@ -482,7 +449,7 @@ void loop() {
   }
 
   if (key3.isClick()) {
-    String * otp3 = TOTP::currentOTP(tfa_seed_3);
+    String *otp3 = TOTP::currentOTP(tfa_seed_3);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
     Keyboard.println(*otp3);
@@ -490,7 +457,7 @@ void loop() {
   }
 
   if (key4.isClick()) {
-    String * otp4 = TOTP::currentOTP(tfa_seed_4);
+    String *otp4 = TOTP::currentOTP(tfa_seed_4);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
     Keyboard.println(*otp4);
@@ -498,7 +465,7 @@ void loop() {
   }
 
   if (key5.isClick()) {
-    String * otp5 = TOTP::currentOTP(tfa_seed_5);
+    String *otp5 = TOTP::currentOTP(tfa_seed_5);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
     Keyboard.println(*otp5);
@@ -508,7 +475,4 @@ void loop() {
   if (key5.isLongClick()) {
     ESP.restart();
   }
-
-
-
 }
--- setup_menu.ino
+++ setup_menu.ino
@@ -1,8 +1,8 @@
 // 2FA Sidecar
 // Matt Perkins - Copyright (C) 2023
 // Spawned out of the need to often type a lot of two factor authentication
-// but still have some security while remaning mostly isolated from the host system.
-// See github for 3D models and wiring diagram.
+// but still have some security while remaning mostly isolated from the host
+// system. See github for 3D models and wiring diagram.
 /*
 
     This program is free software: you can redistribute it and/or modify
@@ -20,13 +20,7 @@
 
 */
 
-
-void setup_test()
-{
-
-
-  int bargraph_pos;
-  long time_x;
+void setup_test() {
   PinButton key1(5);
   PinButton key2(6);
   PinButton key3(9);
@@ -40,7 +34,6 @@ void setup_test()
   delay(500);
   tft.fillScreen(ST77XX_BLUE);
   delay(500);
-
 
   tft.fillScreen(ST77XX_BLACK);
 
@@ -69,7 +62,6 @@ void setup_test()
     key4.update();
     key5.update();
 
-
     if (key1.isClick()) {
       tft.setCursor(65, 18);
       tft.print("OK");
@@ -97,7 +89,7 @@ void setup_test()
       keytest = keytest + 5;
     }
 
-    if (keytest == 15) {
+    if (keytest == maxkeys) {
       tft.setCursor(105, 58);
       tft.print("Test Pass");
       delay(2000);
@@ -106,67 +98,86 @@ void setup_test()
       delay(500);
       tft.setTextColor(ST77XX_WHITE);
       break;
-
-
     }
   }
   wifi_setup();
-
 }
 
-void wifi_setup()
-{
-
+void wifi_setup() {
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid);
 
   tft.setCursor(3, 35);
   tft.fillScreen(ST77XX_RED);
-  tft.printf("Connect via Wifi\nSSID:%s\nthen browse to \nhttp://192.168.4.1", ssid);
-
-
+  tft.printf("Connect via Wifi\nSSID:%s\nthen browse to \nhttp://192.168.4.1",
+             ssid.c_str());
 
   // Send web page with input fields to client
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send_P(200, "text/html", "<HEAD><TITLE>Matt's 2FA-Sidecar </TITLE></HEAD><BODY><H2>2FA-Sidecar configuration menu - (C) 2023 Matt Perkins - GPL</H2>"
-                    "You may submit only one option at a time - current settings are not displayed for security.<p>"
-                    "<FORM ACTION=\"/get\">SSID: <input type=\"text\" name=\"ssid\"><input type=\"submit\" value=\"Submit\"></form><br>"
-                    "<FORM ACTION=\"/get\">WiFi Password: <input type=\"text\" name=\"password\"><input type=\"submit\" value=\"Submit\"></form><br>"
-                    "<FORM ACTION=\"/get\">Access PIN (4 digits.blank for none) : <input type=\"text\" name=\"pin\"><input type=\"submit\" value=\"Submit\"></form><br>"
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(
+        200, "text/html",
+        "<HEAD><TITLE>Matt's 2FA-Sidecar </TITLE></HEAD><BODY><H2>2FA-Sidecar "
+        "configuration menu - (C) 2023 Matt Perkins - GPL</H2>"
+        "You may submit only one option at a time - current settings are not "
+        "displayed for security.<p>"
+        "<FORM ACTION=\"/get\">SSID: <input type=\"text\" name=\"ssid\"><input "
+        "type=\"submit\" value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">WiFi Password: <input type=\"text\" "
+        "name=\"password\"><input type=\"submit\" value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">Access PIN (4 digits.blank for none) : <input "
+        "type=\"text\" name=\"pin\"><input type=\"submit\" "
+        "value=\"Submit\"></form><br>"
 
-                    "<FORM ACTION=\"/get\">2FA Key 1 Name : <input type=\"text\" name=\"tfa_name_1\"><input type=\"submit\" value=\"Submit\"></form><br>"
-                    "<FORM ACTION=\"/get\">2FA Key 1 Seed : <input type=\"text\" name=\"tfa_seed_1\"><input type=\"submit\" value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">2FA Key 1 Name : <input type=\"text\" "
+        "name=\"tfa_name_1\"><input type=\"submit\" "
+        "value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">2FA Key 1 Seed : <input type=\"text\" "
+        "name=\"tfa_seed_1\"><input type=\"submit\" "
+        "value=\"Submit\"></form><br>"
 
-                    "<FORM ACTION=\"/get\">2FA Key 2 Name : <input type=\"text\" name=\"tfa_name_2\"><input type=\"submit\" value=\"Submit\"></form><br>"
-                    "<FORM ACTION=\"/get\">2FA Key 2 Seed : <input type=\"text\" name=\"tfa_seed_2\"><input type=\"submit\" value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">2FA Key 2 Name : <input type=\"text\" "
+        "name=\"tfa_name_2\"><input type=\"submit\" "
+        "value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">2FA Key 2 Seed : <input type=\"text\" "
+        "name=\"tfa_seed_2\"><input type=\"submit\" "
+        "value=\"Submit\"></form><br>"
 
-                    "<FORM ACTION=\"/get\">2FA Key 3 Name : <input type=\"text\" name=\"tfa_name_3\"><input type=\"submit\" value=\"Submit\"></form><br>"
-                    "<FORM ACTION=\"/get\">2FA Key 3 Seed : <input type=\"text\" name=\"tfa_seed_3\"><input type=\"submit\" value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">2FA Key 3 Name : <input type=\"text\" "
+        "name=\"tfa_name_3\"><input type=\"submit\" "
+        "value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">2FA Key 3 Seed : <input type=\"text\" "
+        "name=\"tfa_seed_3\"><input type=\"submit\" "
+        "value=\"Submit\"></form><br>"
 
-                    "<FORM ACTION=\"/get\">2FA Key 4 Name : <input type=\"text\" name=\"tfa_name_4\"><input type=\"submit\" value=\"Submit\"></form><br>"
-                    "<FORM ACTION=\"/get\">2FA Key 4 Seed : <input type=\"text\" name=\"tfa_seed_4\"><input type=\"submit\" value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">2FA Key 4 Name : <input type=\"text\" "
+        "name=\"tfa_name_4\"><input type=\"submit\" "
+        "value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">2FA Key 4 Seed : <input type=\"text\" "
+        "name=\"tfa_seed_4\"><input type=\"submit\" "
+        "value=\"Submit\"></form><br>"
 
-                    "<FORM ACTION=\"/get\">2FA Key 5 Name : <input type=\"text\" name=\"tfa_name_5\"><input type=\"submit\" value=\"Submit\"></form><br>"
-                    "<FORM ACTION=\"/get\">2FA Key 5 Seed : <input type=\"text\" name=\"tfa_seed_5\"><input type=\"submit\" value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">2FA Key 5 Name : <input type=\"text\" "
+        "name=\"tfa_name_5\"><input type=\"submit\" "
+        "value=\"Submit\"></form><br>"
+        "<FORM ACTION=\"/get\">2FA Key 5 Seed : <input type=\"text\" "
+        "name=\"tfa_seed_5\"><input type=\"submit\" "
+        "value=\"Submit\"></form><br>"
 
-                    "</BODY></HTML>");
+        "</BODY></HTML>");
   });
 
-
   // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
-  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest * request) {
+  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request) {
     String inputMessage;
     String inputParam;
 
     preferences.begin("2FA_Sidecar", false);
-
 
     // GET input1 value on <ESP_IP>/get?ssid=<inputMessage>
     if (request->hasParam(PARAM_INPUT_1)) {
       inputMessage = request->getParam(PARAM_INPUT_1)->value();
       inputParam = PARAM_INPUT_1;
       preferences.putString("ssid", inputMessage);
-
     }
     // GET input2 value on <ESP_IP>/get?password=<inputMessage>
     else if (request->hasParam(PARAM_INPUT_2)) {
@@ -251,18 +262,15 @@ void wifi_setup()
       preferences.putString("tfa_seed_5", inputMessage);
     }
 
-
-
     else {
       inputMessage = "No message sent";
       inputParam = "none";
     }
 
-
-    request->send(200, "text/html", "Setting preferences ("
-                  + inputParam + ") with value: " + inputMessage +
-                  "<br><a href=\"/\">Return to Home configuration</a>");
-
+    request->send(200, "text/html",
+                  "Setting preferences (" + inputParam +
+                      ") with value: " + inputMessage +
+                      "<br><a href=\"/\">Return to Home configuration</a>");
   });
 
   server.onNotFound(notFound);
@@ -270,9 +278,7 @@ void wifi_setup()
 
   delay(600000);
   ESP.restart();
-
 }
-
 
 void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
